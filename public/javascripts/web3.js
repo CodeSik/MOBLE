d3f50059342b0c369b3014b7d162b1b3b2006902
geonsikSeo <iqeq2328@gmail.com>
2020-12-19 05:23:17 +0900

    ㅏㅏ

--- public/javascripts/web3.js
+++ public/javascripts/web3.js
@@ -1,11 +1,17 @@
 var web3 = new Web3();
 web3.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-const contract_address = "0x7381a4d55D8A466c13bf1b2ba1f317e02b64aE1F";
-const abi = [
+const contract_address = "0x67fbF067dE55F9187b009d5edfd5936D4582d64C";
+const abi =[
 	{
 		"constant": false,
-		"inputs": [],
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "number",
+				"type": "uint256"
+			}
+		],
 		"name": "Deposit",
 		"outputs": [],
 		"payable": true,
@@ -22,11 +28,16 @@ const abi = [
 			},
 			{
 				"internalType": "string",
-				"name": "name",
+				"name": "_homeaddress",
 				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "_itemnumber",
+				"type": "uint256"
 			}
 		],
-		"name": "auctionBidding",
+		"name": "actionGroupBy",
 		"outputs": [],
 		"payable": true,
 		"stateMutability": "payable",
@@ -41,25 +52,217 @@ const abi = [
 				"type": "uint256"
 			}
 		],
-		"name": "auctionEnd",
+		"name": "balanceTransfer",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
-		"constant": false,
-		"inputs": [
+		"constant": true,
+		"inputs": [],
+		"name": "getAllEndedGroupBuyItems",
+		"outputs": [
 			{
-				"internalType": "address payable",
-				"name": "_receiver",
-				"type": "address"
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "itemnumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "Itemname",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "presentpeopleNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "limitpeopleNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "Owner",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "remaining",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "limitTime",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "startTime",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bool",
+						"name": "ended",
+						"type": "bool"
+					},
+					{
+						"internalType": "address payable[]",
+						"name": "enrolledAd",
+						"type": "address[]"
+					},
+					{
+						"internalType": "string[]",
+						"name": "homeaddresses",
+						"type": "string[]"
+					}
+				],
+				"internalType": "struct GroupBuy.GroupBuyItem[]",
+				"name": "",
+				"type": "tuple[]"
 			}
 		],
-		"name": "balanceTransfer",
-		"outputs": [],
 		"payable": false,
-		"stateMutability": "nonpayable",
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_itemnumber",
+				"type": "uint256"
+			}
+		],
+		"name": "getAmount",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getRegisteredGroupByItems",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "itemnumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "Itemname",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "presentpeopleNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "limitpeopleNumber",
+						"type": "uint256"
+					},
+					{
+						"internalType": "string",
+						"name": "Owner",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256",
+						"name": "price",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "remaining",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "limitTime",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "startTime",
+						"type": "uint256"
+					},
+					{
+						"internalType": "bool",
+						"name": "ended",
+						"type": "bool"
+					},
+					{
+						"internalType": "address payable[]",
+						"name": "enrolledAd",
+						"type": "address[]"
+					},
+					{
+						"internalType": "string[]",
+						"name": "homeaddresses",
+						"type": "string[]"
+					}
+				],
+				"internalType": "struct GroupBuy.GroupBuyItem[]",
+				"name": "",
+				"type": "tuple[]"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getUser",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "string",
+						"name": "Name",
+						"type": "string"
+					},
+					{
+						"internalType": "uint256[]",
+						"name": "Items",
+						"type": "uint256[]"
+					},
+					{
+						"internalType": "address payable",
+						"name": "ad",
+						"type": "address"
+					}
+				],
+				"internalType": "struct GroupBuy.User",
+				"name": "",
+				"type": "tuple"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -71,179 +274,10 @@ const abi = [
 				"type": "uint256"
 			}
 		],
-		"name": "changeItemOwner",
+		"name": "groupBuyEnd",
 		"outputs": [],
 		"payable": false,
 		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getAllAuctionedItems",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "Itemname",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "Owner",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "start_price",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "limit_price",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "remaining",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "limitTime",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "startTime",
-						"type": "uint256"
-					},
-					{
-						"internalType": "bool",
-						"name": "ended",
-						"type": "bool"
-					},
-					{
-						"internalType": "address payable",
-						"name": "add",
-						"type": "address"
-					}
-				],
-				"internalType": "struct IAuction.AuctionItem[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getAllEndedAuctionedItems",
-		"outputs": [
-			{
-				"components": [
-					{
-						"internalType": "string",
-						"name": "Itemname",
-						"type": "string"
-					},
-					{
-						"internalType": "string",
-						"name": "Owner",
-						"type": "string"
-					},
-					{
-						"internalType": "uint256",
-						"name": "start_price",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "limit_price",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "remaining",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "limitTime",
-						"type": "uint256"
-					},
-					{
-						"internalType": "uint256",
-						"name": "startTime",
-						"type": "uint256"
-					},
-					{
-						"internalType": "bool",
-						"name": "ended",
-						"type": "bool"
-					},
-					{
-						"internalType": "address payable",
-						"name": "add",
-						"type": "address"
-					}
-				],
-				"internalType": "struct IAuction.AuctionItem[]",
-				"name": "",
-				"type": "tuple[]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getMyItems",
-		"outputs": [
-			{
-				"internalType": "string[]",
-				"name": "",
-				"type": "string[]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "getName",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "receiver",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
 		"type": "function"
 	},
 	{
@@ -265,39 +299,29 @@ const abi = [
 			},
 			{
 				"internalType": "uint256",
-				"name": "start_price",
+				"name": "_peopleNumber",
 				"type": "uint256"
 			},
 			{
 				"internalType": "uint256",
-				"name": "limit_price",
+				"name": "price",
 				"type": "uint256"
 			},
 			{
 				"internalType": "uint256",
 				"name": "date",
 				"type": "uint256"
-			}
-		],
-		"name": "registerAuctionItem",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"constant": false,
-		"inputs": [
+			},
 			{
 				"internalType": "string",
-				"name": "name",
+				"name": "_homeaddress",
 				"type": "string"
 			}
 		],
-		"name": "registerItem",
+		"name": "registerGroupBuyItem",
 		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
+		"payable": true,
+		"stateMutability": "payable",
 		"type": "function"
 	},
 	{
@@ -320,175 +344,139 @@ let contract = new web3.eth.Contract(abi, contract_address);
 
 
 $(document).ready(function() {
-	startDapp();
+   startDapp();
 })
 
 var startDapp = async function() {
-	getMyItems();
-	getRegisteredAuctionItems();
-	getClosedAuctionItems();
-	getMyItemsToBeAuctioned();
-	getItemsRegisteredAtAuction();
-	getName();
+   getMyItems();
+
+   getRegisteredGroupByItems();
+   getClosedAuctionItems();
+   getMyItemsToBeAuctioned();
+   getItemsRegisteredAtAuction();
+   getName();
 }
 
 
 var getBalance = async function() {
-	var address = await $('#address').text();
-	var name = await contract.methods.getName().call({from:address});
-	web3.eth.getBalance(address, function (error, balance) {
-		if (!error){
-			$('#balanceAmount').text(web3.utils.fromWei(balance,'ether'));
-		}else
-			console.log('error: ', error);
-	});
-	
+   var address = await $('#address').text();
+   web3.eth.getBalance(address, function (error, balance) {
+      if (!error){
+         $('#balanceAmount').text(web3.utils.fromWei(balance,'ether'));
+      }else
+         console.log('error: ', error);
+   });
+   
 }
-
+//ok
 var getName = async function() {
-	var address = await $('#address').text();
-	var name = await contract.methods.getName().call({from:address});
-	$('#name').text(name);
+   var address = await $('#address').text();
+   var name = await contract.methods.getUser().call({from:address});
+   $('#name').text(name[0]);
 }
 
-
-
+//ok
 var registerName = async function() {
-	var address = await $('#address').text();
-	var name = await document.getElementById('change_name').value;
-	try{
-		await contract.methods.registerName(name).send({from : address});
-	}
-	catch(err){
-		alert(err);
-	}
+   var address = await $('#address').text();
+   var name = await document.getElementById('change_name').value;
+   try{
+      await contract.methods.registerName(name).send({from : address});
+   }
+   catch(err){
+      alert(err);
+   }
 }
-
-var registerForMyItem = async function() {
-	var address = await $('#address').text();
-	var item = await document.getElementById('Item').value;
-	
-	try{
-		await contract.methods.registerItem(item).send({from:address});
-	
-	}
-	catch(err){
-		alert(err);
-	}
-}
-var getMyItemsToBeAuctioned = async function() {
-	var address = await $('#address').text();
-	var items = await contract.methods.getMyItems().call({from:address});
-	// items = items.split(",")
-	for(var i=0 ; i<items.length ; i++)
-	{
-		if(items[i] == "" || items[i] == " ")
-		{
-			continue;
-		}
-		option_value = items[i];
-		option_text = items[i];
-		$('#myitems-category').append('<option value="'+ option_value +'">'+ option_text +'</option>');
-	}
-
-}
-var registerAuctionItem = async function() {
+//ok
+var registerGroupBuyItem = async function() {
 	await document.getElementById('change_name').value;
 	var address = await $('#address').text();
 	var items = await document.getElementById('myitems-category').value;
-	var startingBidPrice = await document.getElementById('startingBidPrice').value;
-	var upperLimitPrice = await document.getElementById('upperLimitPrice').value;
+	var price = await document.getElementById('startingBidPrice').value;
+	var _peopleNumber = await document.getElementById('upperLimitPrice').value;
 	var dueDate = await document.getElementById('dueDate').value;
 	try{
-		await contract.methods.registerAuctionItem(items,startingBidPrice,upperLimitPrice,dueDate).send({from:address, gas:5000000});
+	   await contract.methods.registerGroupBuyItem(items,_peopleNumber,price,dueDate,"home").send({from:address, gas:5000000, value:price*Math.pow(10,18)});
 	}
 	catch(err){
-		alert(err)
+	   alert(err)
 	}
-}
+ }
+//ok
+var actionGroupBy = async function() {
+   var address = await $('#address').text();
+   var item = await document.getElementById('auction-category').value;
 
-var auctionBidding = async function() {
-	var address = await $('#address').text();
-	var item = await document.getElementById('auction-category').value;
-	item = String(item).split('/'); //item[0]에 아이템 이름 들어감
-	var bidprice = await document.getElementById('bidPrice').value;
-	try{
-		await contract.methods.auctionBidding(item[0],item[1]).send({from:address,gas:5000000, value : bidprice*Math.pow(10,18)});
-	}
-	catch(err){
-		alert(err);
-	}
+   var price = await document.getElementById('bidPrice').value;
+   try{
+      await contract.methods.actionGroupBy(item,"home", price).send({from:address,gas:5000000, value:2*Math.pow(10,18)});
+   }
+   catch(err){
+      alert(err);
+   }
 }
-
+//ok
 var getMyItems = async function() {
-	var address = await $('#address').text();
-	var items = new Array(); //배열선언
-	items.push(await contract.methods.getMyItems().call({from:address}));
-	// items = String(items).split(',');
-	// for(var i=0;i<items.length;i++){
-	// 	if(items[i] == "" || items[i] == "  ")
-	// 	{
-	// 		items.splice(i,1);
-	// 		i=0;
-	// 	}
-	// }
-	// items.shift();
-	$('#myItems').text(items);
+   var address = await $('#address').text();
+   var items = new Array(); //배열선언
+   //$('#myItems').text(items);
 }
 
-var getRegisteredAuctionItems = async function() {
-	var address = await $('#address').text();
-	// await contract.methods.refreshTime().send({from:address});
-	var AuctionItemInfo = await contract.methods.getAllAuctionedItems().call({from:address});
-	for(var i =0; i<AuctionItemInfo.length ; i++)
-	{
-		if(AuctionItemInfo[i][7] == true){
-			continue;
-		}
-		var rowItem = "<tr>"
-		for(var j=0 ; j<AuctionItemInfo[i].length-4; j++){
-			var element = AuctionItemInfo[i][j];
-			rowItem += "<th>"+element+"</th>";
-		}
-		rowItem += "</tr>"
-		$('#registeredCars').append(rowItem)
-	}
-	
-}
-
-var getClosedAuctionItems = async function() {
-	var address = await $('#address').text();
-	var ClosedItemInfo = await contract.methods.getAllEndedAuctionedItems().call({from:address});
-	for(var i =0; i<ClosedItemInfo.length ; i++)
-	{
-		var rowItem = "<tr>"
-		for(var j=0 ; j<ClosedItemInfo[i].length-6 ; j++){
-			var element = ClosedItemInfo[i][j];
-			rowItem += "<th>"+element+"</th>";
-		}
-		rowItem += "</tr>"
-		$('#carsOnSale').append(rowItem)
-	}
+var getRegisteredGroupByItems = async function() {
+   var address = await $('#address').text();
+   // await contract.methods.refreshTime().send({from:address});
+   var GroupByItemInfo = await contract.methods.getRegisteredGroupByItems().call({from:address});
+   for(var i =0; i<GroupByItemInfo.length ; i++)
+   {
+	   if(GroupByItemInfo[i][9] == true){
+		   continue;
+	   }
+	   var rowItem = "<tr>"
+	   for(var j=0 ; j<GroupByItemInfo[i].length-6; j++){
+		   var element = GroupByItemInfo[i][j];
+		   rowItem += "<th>"+element+"</th>";
+	   }
+	   rowItem += "</tr>"
+	   $('#registeredCars').append(rowItem)
+   }
+   
 }
 
 
 
+//ok
+var getAllEndedGroupByItems = async function() {
+   var address = await $('#address').text();
+   var ClosedItemInfo = await contract.methods.getAllEndedGroupBuyItems().call({from:address});
+   for(var i =0; i<ClosedItemInfo.length ; i++)
+   {
+      var rowItem = "<tr>"
+      rowItem += "<th>" + rowItem[i][1] + "</th>";
+      rowItem += "<th>" + rowItem[i][5] + "</th>";
+      rowItem += "</tr>"
+      $('#carsOnSale').append(rowItem)
+   }
+}
+
+//ok
 var getItemsRegisteredAtAuction = async function() {
-	var address = await $('#address').text();
-	var AuctionItemInfo = await contract.methods.getAllAuctionedItems().call({from:address});
-	for(var i=0 ; i<AuctionItemInfo.length ; i++)
-	{
-		if(AuctionItemInfo[i][7] == true){
-			continue;
-		}
-		var option_value = AuctionItemInfo[i][0];
-		option_value += "/";
-		option_value += AuctionItemInfo[i][1];
+   var address = await $('#address').text();
+   var AuctionItemInfo = await contract.methods.getAllAuctionedItems().call({from:address});
+   for(var i=0 ; i<AuctionItemInfo.length ; i++)
+   {
+      if(AuctionItemInfo[i][7] == true){
+         continue;
+      }
+      var option_value = AuctionItemInfo[i][1];
+      option_value += "/";
+      option_value += AuctionItemInfo[i][5];
+    option_value += AuctionItemInfo[i][10].length;
 
-		var option_text = AuctionItemInfo[i][0];
-		option_text += "/" ;
-		option_text += AuctionItemInfo[i][1];
+      var option_text = AuctionItemInfo[i][1];
+      option_text += "/" ;
+      option_text += AuctionItemInfo[i][5];
+      // number of participants
+      option_text += AuctionItemInfo[i][10].length;
 
-		$('#auction-category').append('<option value="'+ option_value +'">'+ option_text +'</option>');
-	}
+      $('#auction-category').append('<option value="'+ option_value +'">'+ option_text +'</option>');
+   }
 }
